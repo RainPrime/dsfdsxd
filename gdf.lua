7a7b580b82a61002aaac146a46ed714fecca1860
RainPrime <ggfff4726@gmail.com>
2024-11-15 00:00:50 +0700

    Update gdf.lua

--- gdf.lua
+++ gdf.lua
@@ -1,18 +1,1 @@
-https://raw.githubusercontent.com/RainPrime/Rain4.0/refs/heads/main/sd.lua
-L_5_.Info:AddButton(
-        {
-            Title = "Thank for use _Hub",
-            Description = "dev by melon x sp by TThe",
-            Callback = function()
-                setclipboard("dev by melon x sp by TThe")
-                Fluent:Notify(
-                    {
-                        Title = "melon x_Hub",
-                        Content = "dev by Rain  sp by TThe",
-                        SubContent = "", -- Optional
-                        Duration = 5 -- Set to nil to make the notification not disappear
-                    }
-                )
-            end
-        }
-    )
+loadstring(game:HttpGet("https://raw.githubusercontent.com/RainPrime/Rain4.0/refs/heads/main/sd.lua"))()
